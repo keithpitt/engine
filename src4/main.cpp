5fbe483fe2e8be4692c63dc787b5fd06e61a7c00
Keith Pitt <me@keithpitt.com>
2013-01-22 08:55:45 +1100

    Started creating a shader, :(

--- src4/main.cpp
+++ src4/main.cpp
@@ -20,6 +20,50 @@ static void error_callback(int error, const char* description)
     fprintf(stderr, "Error: %s\n", description);
 }
 
+class Shader {
+public:
+    
+    const char* source;
+    GLenum type;
+    
+    Shader(const char* source, GLenum shaderType) {
+        this->source = source;
+        this->type = shaderType;
+        
+        // Compile the shader
+        compile();
+    };
+    
+    void compile() {
+        // Create a shader of the type specified
+        GLuint shader = glCreateShader(type);
+        if(shader == 0)
+            throw std::runtime_error("glCreateShader failed");
+        
+        // Send our shader source into the shader.
+        glShaderSource(shader, 1, (const GLchar**)&source, NULL);
+        
+        // Actaully compile the shader now.
+        glCompileShader(shader);
+        
+        // Find the status of the compilation
+        GLint status;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+        
+        if(status != GL_TRUE) {
+            // Grab the shader log
+            char buffer[1024];
+            glGetShaderInfoLog(shader, sizeof(buffer), NULL, buffer);
+            
+            // Output and exit
+            printf("Error:  Failed to compile buffer:\n%s", buffer);
+            
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+};
+
 int main(void)
 {
     GLFWwindow* window;
@@ -60,14 +104,31 @@ int main(void)
     printf("GLSL version: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
     printf("Vendor: %s\n", glGetString(GL_VENDOR));
     printf("Renderer: %s\n", glGetString(GL_RENDERER));
-    
-    GLuint vertexBuffer;
-    glGenBuffers( 1, &vertexBuffer );
-    
-    printf( "%u\n", vertexBuffer );
 
     // Ensure we can capture the escape key being pressed below
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    
+    // Define the vertices for our triangle
+    float vertices[] = {
+        0.0f,  0.5f,
+        0.5f, -0.5f,
+       -0.5f, -0.5f
+    };
+    
+    // Create a vertex buffer
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    
+    // Make the vbo the current vertext buffer object. We also tell
+    // OpenGL that the VBO contains an array!
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    
+    // Load the vertices array into the current vertext buffer object.
+    // Note: GL_STATIC_DRAW: The vertex data will be uploaded once and drawn many times (e.g. the world)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    Shader * shader = new Shader("#version 120\n\nattribute vec2 position;\n\nvoid main() { \n gl_Position = vec4( position, 0.0, 1.0 ); \n }\n", GL_VERTEX_SHADER);
+    delete shader;
 
     for (;;)
     {
@@ -80,7 +141,7 @@ int main(void)
         glfwPollEvents();
 
         // Check if the ESC key was pressed or the window should be closed
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) || glfwGetKey(window, GLFW_KEY_ENTER))
             break;
         if (glfwGetWindowParam(window, GLFW_SHOULD_CLOSE))
             break;
