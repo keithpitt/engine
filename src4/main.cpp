0cfa71ef49fa4a68355726d56846656d31161ada
Keith Pitt <me@keithpitt.com>
2013-01-21 21:05:47 +1100

    Getting GLEW working.

--- src4/main.cpp
+++ src4/main.cpp
@@ -6,10 +6,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdexcept>
 
 #define GLFW_DLL
 #define GLFW_INCLUDE_GLU
 #include <GL/glew.h>
+
 #include <GL/glfw3.h>
 #include <glm/glm.hpp>
 
@@ -20,74 +22,58 @@ static void error_callback(int error, const char* description)
 
 int main(void)
 {
-    int width, height, x;
     GLFWwindow* window;
-    GLFWmonitor* monitor;
 
     glfwSetErrorCallback(error_callback);
 
     // Initialise GLFW
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-
-    monitor = glfwGetPrimaryMonitor();
-
-    // Open a window and create its OpenGL context
-    window = glfwCreateWindow(1440, 900, "Spinning Triangle", monitor, NULL);
-    if (!window)
-    {
-        glfwTerminate();
+    if (!glfwInit()) {
         exit(EXIT_FAILURE);
     }
 
-    // Enable vertical sync (on cards that support it)
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+    // Open a window and create its OpenGL context
+    window = glfwCreateWindow(800, 600, "Mandrill", NULL, NULL);
+    if (!window) {
+        throw std::runtime_error("glfwCreateWindow failed. Can your hardware handle OpenGL 3.2?");
+    }
+    
+    // Create an OpenGL context on the window we've just created
     glfwMakeContextCurrent(window);
+    
+    // Enable vertical sync (on cards that support it)
     glfwSwapInterval(1);
+    
+    // Initialise GLEW
+    glewExperimental = GL_TRUE; //stops glew crashing on OSX :-/
+    GLenum err = glewInit();
+    if(err != GLEW_OK) {
+        printf("glewInit failed: %s\n", glewGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    // print out some info about the graphics drivers
+    printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+    printf("GLSL version: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+    printf("Vendor: %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    
+    GLuint vertexBuffer;
+    glGenBuffers( 1, &vertexBuffer );
+    
+    printf( "%u\n", vertexBuffer );
 
     // Ensure we can capture the escape key being pressed below
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
     for (;;)
     {
-        double t = glfwGetTime();
-        glfwGetCursorPos(window, &x, NULL);
-
-        // Get window size (may be different than the requested size)
-        glfwGetWindowSize(window, &width, &height);
-
-        // Special case: avoid division by zero below
-        height = height > 0 ? height : 1;
-
-        glViewport(0, 0, width, height);
-
         // Clear color buffer to black
         glClearColor(0.f, 0.f, 0.f, 0.f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // Select and setup the projection matrix
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        gluPerspective(65.f, (GLfloat) width / (GLfloat) height, 1.f, 100.f);
-
-        // Select and setup the modelview matrix
-        glMatrixMode( GL_MODELVIEW );
-        glLoadIdentity();
-        gluLookAt(0.f, 1.f, 0.f,    // Eye-position
-                  0.f, 20.f, 0.f,   // View-point
-                  0.f, 0.f, 1.f);   // Up-vector
-
-        // Draw a rotating colorful triangle
-        glTranslatef(0.f, 14.f, 0.f);
-        glRotatef(0.3f * (GLfloat) x + (GLfloat) t * 100.f, 0.f, 0.f, 1.f);
-
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-5.f, 0.f, -4.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(5.f, 0.f, -4.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.f, 6.f);
-        glEnd();
 
         // Swap buffers
         glfwSwapBuffers(window);
