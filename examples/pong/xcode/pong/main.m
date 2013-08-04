9bcbb02bd40b81a211f2342bf6cd40d0c39b5050
Keith Pitt <me@keithpitt.com>
2013-08-04 17:29:16 +1000

    Added empty Xcode project.

---
+++ examples/pong/xcode/pong/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  pong
+//
+//  Created by Keith Pitt on 4/08/13.
+//  Copyright (c) 2013 Keith Pitt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
