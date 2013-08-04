d95997368448fca3cd528c5f937a120d22af5a3d
Keith Pitt <me@keithpitt.com>
2013-08-04 17:52:14 +1000

    An xcode project is now compiling.

---
+++ examples/pong/xcode/Pong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pong
+//
+//  Created by Keith Pitt on 4/08/13.
+//  Copyright (c) 2013 Keith Pitt. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ examples/pong/xcode/Pong/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  Pong
+//
+//  Created by Keith Pitt on 4/08/13.
+//  Copyright (c) 2013 Keith Pitt. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
